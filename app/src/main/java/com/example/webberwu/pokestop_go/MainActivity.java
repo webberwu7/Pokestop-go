0f14f1e258648a9a432387a6efada504a72eb676
webberwu7 <nmmn1501@gmail.com>
2016-10-23 14:04:40 +0800

    push all

---
+++ app/src/main/java/com/example/webberwu/pokestop_go/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.webberwu.pokestop_go;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
